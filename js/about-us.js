b23b77c8fee79feb563e7d3c5e2e1e050f177971
Yazandar <yazanadd@gmail.com>
2021-05-03 15:52:59 +0300

    style the layout and put background

--- js/about-us.js
+++ js/about-us.js
@@ -1,17 +1,17 @@
 'use strict';
-let infos = document.getElementsByClassName('info');
-let image = document.getElementsByClassName('studentImg')
-let show = document.getElementById('detailsButton')
+// let infos = document.getElementsByClassName('info');
+
+// let show = document.getElementById('detailsButton')
 
 
-    for( let i=0; i< infos.length; i++){
-        infos[i].style.display='none';
-    }
+//     for( let i=0; i< infos.length; i++){
+//         infos[i].style.display='none';
+//     }
 
 
-function details(){
-    for( let i=0; i<infos.length; i++){
-        infos[i].style.display='block';
-        show.innerHTML='';
-    }
-}
+// function details(){
+//     for( let i=0; i<infos.length; i++){
+//         infos[i].style.display='block';
+//         show.innerHTML='';
+//     }
+// }
